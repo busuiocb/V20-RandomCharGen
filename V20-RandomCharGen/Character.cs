6c6c6faf52d40c9c629cec647a060cfa63812eb3
B Busuioc <busuiocb@me.com>
2019-10-08 16:46:04 +0100

    Fixed issue with randomising attribute point allocation.

--- V20-RandomCharGen/Character.cs
+++ V20-RandomCharGen/Character.cs
@@ -35,20 +35,20 @@ namespace V20_RandomCharGen
 
 
 
-    public static int Limit(int value, int min, int max)
-    {
-        if (value > max) { value = max; }
-        if (value < min) { value = min; }
-        return value;
-    }
-    private void GenerateAbilities()
+        public static int Limit(int value, int min, int max)
+        {
+            if (value > max) { value = max; }
+            if (value < min) { value = min; }
+            return value;
+        }
+        private void GenerateAbilities()
         {
 
 
             for (int i = 0; i < 3; i++)
             {
-                int index = rand.Next(0, abilityGroupValuesArray.Count - 1);
-                int index1 = rand.Next(0, abilityGroupArray.Count - 1);
+                int index = rand.Next(0, abilityGroupValuesArray.Count);
+                int index1 = rand.Next(0, abilityGroupArray.Count);
                 abilityGroupArray[index1].currentPoints = abilityGroupValuesArray[index];
                 abilityGroupArray[index1].maximum = 5;
                 list.Add(abilityGroupArray[index1]);
@@ -60,28 +60,31 @@ namespace V20_RandomCharGen
             foreach (AbilityGroup ab in list) 
             {
                 bool flag = false;
-                int curPoints = ab.currentPoints; 
+                int curPoints = ab.currentPoints;
+ 
                 while (!flag)
                 {
 
-                    int r1 = rand.Next(0, ab.maximum);
+                    int r1 = rand.Next(0, ab.maximum); 
                     r1 = Limit(r1, 0, curPoints);
-                    ab.Attribute1Val += r1;
+                    ab.Attribute1Val += r1;             
                     curPoints -= r1;
 
                     int r2 = rand.Next(0, ab.maximum);
                     r2 = Limit(r2, 0, curPoints);
                     ab.Attribute2Val += r2;
                     curPoints -= r2;
-
+                    
                     int r3 = rand.Next(0, ab.maximum);
                     r3 = Limit(r3, 0, curPoints);
                     ab.Attribute3Val += r3;
                     curPoints -= r3;
-
                     flag = (curPoints <= 0);
 
+
+
                 }
+
                 ab.Attribute1Val++;
                 ab.Attribute2Val++;
                 ab.Attribute3Val++;

13f1917e1448e62c52e4915886666c1732afa0b0
B Busuioc <busuiocb@me.com>
2019-10-08 14:56:30 +0100

    Updated variable names

--- V20-RandomCharGen/Character.cs
+++ V20-RandomCharGen/Character.cs
@@ -9,7 +9,7 @@ namespace V20_RandomCharGen
 
     class Character
     {
-        class Ability
+        class AbilityGroup
         {
             public int maximum;
             public int currentPoints;
@@ -17,25 +17,25 @@ namespace V20_RandomCharGen
             public int Attribute2Val;
             public int Attribute3Val;
         }
-        Ability physical = new Ability();
-        Ability social = new Ability();
-        Ability mental = new Ability();
-        List<int> abilityValuesArray = new List<int>(new int[] { 7, 5, 3 });
-        List<Ability> abilitiesArray = new List<Ability>();
-        readonly List<Ability> list = new List<Ability>();
+        readonly AbilityGroup physical = new AbilityGroup();
+        readonly AbilityGroup social = new AbilityGroup();
+        readonly AbilityGroup mental = new AbilityGroup();
+        readonly List<int> abilityGroupValuesArray = new List<int>(new int[] { 7, 5, 3 });
+        readonly List<AbilityGroup> abilityGroupArray = new List<AbilityGroup>();
+        readonly List<AbilityGroup> list = new List<AbilityGroup>();
         private readonly Random rand = new Random();
 
         public Character()
         {
-            abilitiesArray.Add(physical);
-            abilitiesArray.Add(social);
-            abilitiesArray.Add(mental);
+            abilityGroupArray.Add(physical);
+            abilityGroupArray.Add(social);
+            abilityGroupArray.Add(mental);
             GenerateAbilities();
         }
 
 
 
-    public static int limit(int value, int min, int max)
+    public static int Limit(int value, int min, int max)
     {
         if (value > max) { value = max; }
         if (value < min) { value = min; }
@@ -47,17 +47,17 @@ namespace V20_RandomCharGen
 
             for (int i = 0; i < 3; i++)
             {
-                int index = rand.Next(0, abilityValuesArray.Count - 1);
-                int index1 = rand.Next(0, abilitiesArray.Count - 1);
-                abilitiesArray[index1].currentPoints = abilityValuesArray[index];
-                abilitiesArray[index1].maximum = 5;
-                list.Add(abilitiesArray[index1]);
-                abilitiesArray.RemoveAt(index1);
-                abilityValuesArray.RemoveAt(index);
+                int index = rand.Next(0, abilityGroupValuesArray.Count - 1);
+                int index1 = rand.Next(0, abilityGroupArray.Count - 1);
+                abilityGroupArray[index1].currentPoints = abilityGroupValuesArray[index];
+                abilityGroupArray[index1].maximum = 5;
+                list.Add(abilityGroupArray[index1]);
+                abilityGroupArray.RemoveAt(index1);
+                abilityGroupValuesArray.RemoveAt(index);
 
             }
 
-            foreach (Ability ab in list) 
+            foreach (AbilityGroup ab in list) 
             {
                 bool flag = false;
                 int curPoints = ab.currentPoints; 
@@ -65,19 +65,19 @@ namespace V20_RandomCharGen
                 {
 
                     int r1 = rand.Next(0, ab.maximum);
-                    r1 = limit(r1, 0, curPoints);
-                    ab.Attribute1Val = ab.Attribute1Val + r1;
-                    curPoints = curPoints - r1;
+                    r1 = Limit(r1, 0, curPoints);
+                    ab.Attribute1Val += r1;
+                    curPoints -= r1;
 
                     int r2 = rand.Next(0, ab.maximum);
-                    r2 = limit(r2, 0, curPoints);
-                    ab.Attribute2Val = ab.Attribute2Val + r2;
-                    curPoints = curPoints - r2;
+                    r2 = Limit(r2, 0, curPoints);
+                    ab.Attribute2Val += r2;
+                    curPoints -= r2;
 
                     int r3 = rand.Next(0, ab.maximum);
-                    r3 = limit(r3, 0, curPoints);
-                    ab.Attribute3Val = ab.Attribute3Val + r3;
-                    curPoints = curPoints - r3;
+                    r3 = Limit(r3, 0, curPoints);
+                    ab.Attribute3Val += r3;
+                    curPoints -= r3;
 
                     flag = (curPoints <= 0);
 

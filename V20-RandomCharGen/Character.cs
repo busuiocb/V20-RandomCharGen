108ebde3a17c32251b438b3086e21a10c1f6bec2
B Busuioc <busuiocb@me.com>
2019-10-18 15:12:38 +0100

    Added abilities text boxes and abilities randomizer code.

--- V20-RandomCharGen/Character.cs
+++ V20-RandomCharGen/Character.cs
@@ -17,20 +17,52 @@ namespace V20_RandomCharGen
             public int Attribute2Val;
             public int Attribute3Val;
         }
+        public class AbilityGroup
+        {
+            public int maximum;
+            public int currentPoints;
+            public int Ability1Val;
+            public int Ability2Val;
+            public int Ability3Val;
+            public int Ability4Val;
+            public int Ability5Val;
+            public int Ability6Val;
+            public int Ability7Val;
+            public int Ability8Val;
+            public int Ability9Val;
+            public int Ability10Val;
+
+        }
+        //Attribute Variables
         readonly AttributeGroup physical = new AttributeGroup();
         readonly AttributeGroup social = new AttributeGroup();
         readonly AttributeGroup mental = new AttributeGroup();
         readonly List<int> AttributeGroupValuesArray = new List<int>(new int[] { 7, 5, 3 });
         readonly List<AttributeGroup> AttributeGroupArray = new List<AttributeGroup>();
-        readonly List<AttributeGroup> list = new List<AttributeGroup>();
-        private readonly Random rand = new Random();
+        readonly List<AttributeGroup> AttributeList = new List<AttributeGroup>();
+        private readonly Random randAttribute = new Random();
+
+        //Aility Variables
+        readonly AbilityGroup talents = new AbilityGroup();
+        readonly AbilityGroup skills = new AbilityGroup();
+        readonly AbilityGroup knowledges = new AbilityGroup();
+        readonly List<int> AbilityGroupValuesArray = new List<int>(new int[] { 13, 9, 5 });
+        readonly List<AbilityGroup> AbilityGroupArray = new List<AbilityGroup>();
+        readonly List<AbilityGroup> AbilityList = new List<AbilityGroup>();
+        private readonly Random randAbility = new Random();
+
+
 
         public Character()
         {
             AttributeGroupArray.Add(physical);
             AttributeGroupArray.Add(social);
             AttributeGroupArray.Add(mental);
-            
+
+            AbilityGroupArray.Add(talents);
+            AbilityGroupArray.Add(skills);
+            AbilityGroupArray.Add(knowledges);
+
         }
 
 
@@ -49,18 +81,18 @@ namespace V20_RandomCharGen
             //Assign priority order to each attribute group
             for (int i = 0; i < 3; i++)
             {
-                int index = rand.Next(0, AttributeGroupValuesArray.Count);
-                int index1 = rand.Next(0, AttributeGroupArray.Count);
+                int index = randAttribute.Next(0, AttributeGroupValuesArray.Count);
+                int index1 = randAttribute.Next(0, AttributeGroupArray.Count);
                 AttributeGroupArray[index1].currentPoints = AttributeGroupValuesArray[index];
                 AttributeGroupArray[index1].maximum = 4;
-                list.Add(AttributeGroupArray[index1]);
+                AttributeList.Add(AttributeGroupArray[index1]);
                 AttributeGroupArray.RemoveAt(index1);
                 AttributeGroupValuesArray.RemoveAt(index);
 
             }
 
             //Generate a random value for each attribute based on the priority assigned previously
-            foreach (AttributeGroup att in list) 
+            foreach (AttributeGroup att in AttributeList) 
             {
                 bool flag = false;
                 int curPoints = att.currentPoints;
@@ -68,17 +100,17 @@ namespace V20_RandomCharGen
                 while (!flag)
                 {
 
-                    int r1 = rand.Next(0, att.maximum); 
+                    int r1 = randAttribute.Next(0, att.maximum); 
                     r1 = Limit(r1, 0, curPoints);
                     att.Attribute1Val =  Limit(r1 + att.Attribute1Val, 0, att.maximum);
                     curPoints -= r1;
 
-                    int r2 = rand.Next(0, att.maximum);
+                    int r2 = randAttribute.Next(0, att.maximum);
                     r2 = Limit(r2, 0, curPoints);
                     att.Attribute2Val = Limit(r2 + att.Attribute2Val, 0, att.maximum); ;
                     curPoints -= r2;
                     
-                    int r3 = rand.Next(0, att.maximum);
+                    int r3 = randAttribute.Next(0, att.maximum);
                     r3 = Limit(r3, 0, curPoints);
                     att.Attribute3Val = Limit(r3 + att.Attribute3Val, 0, att.maximum);
                     curPoints -= r3;
@@ -96,11 +128,97 @@ namespace V20_RandomCharGen
 
             }
 
-            return list;
+            return AttributeList;
 
 
         }
+        //Generate random abilities
+        public List<AbilityGroup> GenerateAbilities()
+        {
 
+            //Assign priority order to each ability group
+            for (int i = 0; i < 3; i++)
+            {
+                int index = randAbility.Next(0, AbilityGroupValuesArray.Count);
+                int index1 = randAbility.Next(0, AbilityGroupValuesArray.Count);
+                AbilityGroupArray[index1].currentPoints = AbilityGroupValuesArray[index];
+                AbilityGroupArray[index1].maximum = 3;
+                AbilityList.Add(AbilityGroupArray[index1]);
+                AbilityGroupArray.RemoveAt(index1);
+                AbilityGroupValuesArray.RemoveAt(index);
+
+            }
+
+            //Generate a random value for each ability based on the priority assigned previously
+            foreach (AbilityGroup ab in AbilityList)
+            {
+                bool flag = false;
+                int curPoints = ab.currentPoints;
+
+                while (!flag)
+                {
+
+                    int r1 = randAttribute.Next(0, ab.maximum);
+                    r1 = Limit(r1, 0, curPoints);
+                    ab.Ability1Val = Limit(r1 + ab.Ability1Val, 0, ab.maximum);
+                    curPoints -= r1;
+
+                    int r2 = randAttribute.Next(0, ab.maximum);
+                    r2 = Limit(r2, 0, curPoints);
+                    ab.Ability2Val = Limit(r2 + ab.Ability2Val, 0, ab.maximum); ;
+                    curPoints -= r2;
+
+                    int r3 = randAttribute.Next(0, ab.maximum);
+                    r3 = Limit(r3, 0, curPoints);
+                    ab.Ability3Val = Limit(r3 + ab.Ability3Val, 0, ab.maximum);
+                    curPoints -= r3;
+
+                    int r4 = randAttribute.Next(0, ab.maximum);
+                    r4 = Limit(r4, 0, curPoints);
+                    ab.Ability4Val = Limit(r4 + ab.Ability4Val, 0, ab.maximum);
+                    curPoints -= r4;
+
+                    int r5 = randAttribute.Next(0, ab.maximum);
+                    r5 = Limit(r5, 0, curPoints);
+                    ab.Ability5Val = Limit(r5 + ab.Ability5Val, 0, ab.maximum);
+                    curPoints -= r5;
+
+                    int r6 = randAttribute.Next(0, ab.maximum);
+                    r6 = Limit(r6, 0, curPoints);
+                    ab.Ability6Val = Limit(r6 + ab.Ability6Val, 0, ab.maximum);
+                    curPoints -= r6;
+
+                    int r7 = randAttribute.Next(0, ab.maximum);
+                    r7 = Limit(r7, 0, curPoints);
+                    ab.Ability7Val = Limit(r7 + ab.Ability7Val, 0, ab.maximum);
+                    curPoints -= r7;
+
+                    int r8 = randAttribute.Next(0, ab.maximum);
+                    r8 = Limit(r8, 0, curPoints);
+                    ab.Ability8Val = Limit(r8 + ab.Ability8Val, 0, ab.maximum);
+                    curPoints -= r8;
+
+                    int r9 = randAttribute.Next(0, ab.maximum);
+                    r9 = Limit(r9, 0, curPoints);
+                    ab.Ability9Val = Limit(r9 + ab.Ability9Val, 0, ab.maximum);
+                    curPoints -= r9;
+
+                    int r10 = randAttribute.Next(0, ab.maximum);
+                    r10 = Limit(r10, 0, curPoints);
+                    ab.Ability10Val = Limit(r10 + ab.Ability10Val, 0, ab.maximum);
+                    curPoints -= r10;
+                    flag = (curPoints <= 0);
+
+
+
+                }
+
+            }
+
+            return AbilityList;
+
+
+        }
 
 
 
--- V20-RandomCharGen/MainWindow.xaml.cs
+++ V20-RandomCharGen/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace V20_RandomCharGen
             InitializeComponent();
 
 
-
+          
 
         }
 
@@ -33,22 +33,64 @@ namespace V20_RandomCharGen
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             Character ion = new Character();
-            List<Character.AttributeGroup> test = ion.GenerateAttributes();
+            List<Character.AttributeGroup> rollAttributes = ion.GenerateAttributes();
+            List<Character.AbilityGroup> rollAbilities = ion.GenerateAbilities();
 
             //Write Values to Physical Attributes
-            textbox1.Text = test[0].Attribute1Val.ToString();
-            textbox2.Text = test[0].Attribute2Val.ToString();
-            textbox3.Text = test[0].Attribute3Val.ToString();
+            textbox1.Text = rollAttributes[0].Attribute1Val.ToString();
+            textbox2.Text = rollAttributes[0].Attribute2Val.ToString();
+            textbox3.Text = rollAttributes[0].Attribute3Val.ToString();
 
             //Write Values to Social Attributes
-            textbox4.Text = test[1].Attribute1Val.ToString();
-            textbox5.Text = test[1].Attribute2Val.ToString();
-            textbox6.Text = test[1].Attribute3Val.ToString();
+            textbox4.Text = rollAttributes[1].Attribute1Val.ToString();
+            textbox5.Text = rollAttributes[1].Attribute2Val.ToString();
+            textbox6.Text = rollAttributes[1].Attribute3Val.ToString();
 
             //Write Values to Mental Attributes
-            textbox7.Text = test[2].Attribute1Val.ToString();
-            textbox8.Text = test[2].Attribute2Val.ToString();
-            textbox9.Text = test[2].Attribute3Val.ToString();
+            textbox7.Text = rollAttributes[2].Attribute1Val.ToString();
+            textbox8.Text = rollAttributes[2].Attribute2Val.ToString();
+            textbox9.Text = rollAttributes[2].Attribute3Val.ToString();
+
+
+            //Write Values to Talent Abilities
+            textbox10.Text = rollAbilities[0].Ability1Val.ToString();
+            textbox11.Text = rollAbilities[0].Ability2Val.ToString();
+            textbox12.Text = rollAbilities[0].Ability3Val.ToString();
+            textbox13.Text = rollAbilities[0].Ability4Val.ToString();
+            textbox14.Text = rollAbilities[0].Ability5Val.ToString();
+            textbox15.Text = rollAbilities[0].Ability6Val.ToString();
+            textbox16.Text = rollAbilities[0].Ability7Val.ToString();
+            textbox17.Text = rollAbilities[0].Ability8Val.ToString();
+            textbox18.Text = rollAbilities[0].Ability9Val.ToString();
+            textbox19.Text = rollAbilities[0].Ability10Val.ToString();
+
+            //Write Values to Skill Abilities
+            textbox20.Text = rollAbilities[1].Ability1Val.ToString();
+            textbox21.Text = rollAbilities[1].Ability2Val.ToString();
+            textbox22.Text = rollAbilities[1].Ability3Val.ToString();
+            textbox23.Text = rollAbilities[1].Ability4Val.ToString();
+            textbox24.Text = rollAbilities[1].Ability5Val.ToString();
+            textbox25.Text = rollAbilities[1].Ability6Val.ToString();
+            textbox26.Text = rollAbilities[1].Ability7Val.ToString();
+            textbox27.Text = rollAbilities[1].Ability8Val.ToString();
+            textbox28.Text = rollAbilities[1].Ability9Val.ToString();
+            textbox29.Text = rollAbilities[1].Ability10Val.ToString();
+
+            //Write Values to Knowledge Abilities
+            textbox30.Text = rollAbilities[2].Ability1Val.ToString();
+            textbox31.Text = rollAbilities[2].Ability2Val.ToString();
+            textbox32.Text = rollAbilities[2].Ability3Val.ToString();
+            textbox33.Text = rollAbilities[2].Ability4Val.ToString();
+            textbox34.Text = rollAbilities[2].Ability5Val.ToString();
+            textbox35.Text = rollAbilities[2].Ability6Val.ToString();
+            textbox36.Text = rollAbilities[2].Ability7Val.ToString();
+            textbox37.Text = rollAbilities[2].Ability8Val.ToString();
+            textbox38.Text = rollAbilities[2].Ability9Val.ToString();
+            textbox39.Text = rollAbilities[2].Ability10Val.ToString();
+
+
+
+
         }
     }
 }
